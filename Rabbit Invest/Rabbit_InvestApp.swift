392f0050706d3fe42ba189ec3f437c9377283fd6
Kanav Nijhawan <nijhawankanav@gmail.com>
2025-08-20 14:31:41 +0530

    Initial Commit

---
+++ Rabbit Invest/Rabbit_InvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rabbit_InvestApp.swift
+//  Rabbit Invest
+//
+//  Created by Kanav  Nijhawan on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Rabbit_InvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
